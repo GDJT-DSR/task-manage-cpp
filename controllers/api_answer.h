18a4d04aab50e4e9e7d13c6894d993301c658f13
GDJT-DSR <dsr15546488520@126.com>
2026-08-24 21:44:01 +0800

    change

--- controllers/api_answer.h
+++ controllers/api_answer.h
@@ -4,50 +4,49 @@
 
 using namespace drogon;
 
-namespace dto
-{
-    struct TextSubmit
-    {
-        std::string target;
-        std::string origin;
-    };
+namespace dto {
+struct TextSubmit {
+    std::string target;
+    std::string origin;
+};
 } // namespace dto
 
-namespace drogon
-{
-    template <>
-    inline std::optional<dto::TextSubmit> fromRequest(const HttpRequest& req)
-    {
-        const auto& json = req.getJsonObject();
-        if (!json) { return std::nullopt; }
-
-        if (const Json::Value &target = (*json)["target"],
-                              &origin = (*json)["origin"];
-            target.isString() && origin.isString()) { return dto::TextSubmit{target.asString(), origin.asString()}; }
+namespace drogon {
+template <>
+inline std::optional<dto::TextSubmit> fromRequest(const HttpRequest &req) {
+    const auto &json = req.getJsonObject();
+    if (!json) {
         return std::nullopt;
     }
+
+    if (const Json::Value &target = (*json)["target"],
+        &origin = (*json)["origin"];
+        target.isString() && origin.isString()) {
+        return dto::TextSubmit{target.asString(), origin.asString()};
+    }
+    return std::nullopt;
+}
 } // namespace drogon
 
-namespace api
-{
-    class answer : public drogon::HttpController<answer>
-    {
-    public:
-        METHOD_LIST_BEGIN
-            METHOD_ADD(answer::text, "/{id}/fill_in", Post);
-            METHOD_ADD(answer::image, "/{id}/image", Post);
-            METHOD_ADD(answer::choose, "/{id}/choose", Post);
-        METHOD_LIST_END
+namespace api {
+class answer : public drogon::HttpController<answer> {
+  public:
+    METHOD_LIST_BEGIN
+    METHOD_ADD(answer::text, "/{id}/fill_in", Post);
+    METHOD_ADD(answer::image, "/{id}/image", Post);
+    METHOD_ADD(answer::choose, "/{id}/choose", Post);
+    METHOD_LIST_END
 
-        static Task<> text(HttpRequestPtr req,
-                           std::function<void(const HttpResponsePtr&)> callback,
-                           int question_id, std::optional<dto::TextSubmit> data);
-        static Task<> image(HttpRequestPtr req,
-                            std::function<void(const HttpResponsePtr&)> callback,
-                            int32_t id);
-        static Task<> choose(HttpRequestPtr req,
-                             std::function<void(const HttpResponsePtr&)> callback,
-                             int question_id, std::optional<dto::TextSubmit> data);
-        static std::string validate(const orm::Row&, const std::string&, bool change);
-    };
+    static Task<> text(HttpRequestPtr req,
+                       std::function<void(const HttpResponsePtr &)> callback,
+                       int question_id, std::optional<dto::TextSubmit> data);
+    static Task<> image(HttpRequestPtr req,
+                        std::function<void(const HttpResponsePtr &)> callback,
+                        int32_t id);
+    static Task<> choose(HttpRequestPtr req,
+                         std::function<void(const HttpResponsePtr &)> callback,
+                         int question_id, std::optional<dto::TextSubmit> data);
+    static std::string validate(const orm::Row &, const std::string &,
+                                bool change);
+};
 } // namespace api
